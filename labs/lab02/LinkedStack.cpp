19e61c39f01e6dbdc219df28ab8c13a1d95b254b
jay ess <mentoce@gmail.com>
2019-10-04 19:21:17 -0700

    Fix formatting and such

--- labs/lab02/LinkedStack.cpp
+++ labs/lab02/LinkedStack.cpp
@@ -1,84 +1,102 @@
-/** @file LinkedStack.cpp */
-#include <cassert>       // For assert
-#include "LinkedStack.h" // Header file
-template <class ItemType>
-LinkedStack<ItemType>::LinkedStack() : topPtr(nullptr)
+#include <cassert>
+#include "LinkedStack.h"
+
+/** Constructor */
+template <class T>
+LinkedStack<T>::LinkedStack() : topPtr(nullptr)
 {
-} // end default constructor
-template <class ItemType>
-LinkedStack<ItemType>::
-    LinkedStack(const LinkedStack<ItemType> &aStack)
+}
+
+/** Copy constructor */
+template <class T>
+LinkedStack<T>::
+    LinkedStack(const LinkedStack<T> &aStack)
 {
     // Point to nodes in original chain
-    Node<ItemType> *origChainPtr = aStack->topPtr;
+    Node<T> *origChainPtr = aStack->topPtr;
     if (origChainPtr == nullptr)
         topPtr = nullptr; // Original bag is empty
     else
     {
         // Copy first node
-        topPtr = new Node<ItemType>();
-        topPtr->setItem(origChainPtr->getItem());
+        topPtr = new Node<T>();
+        topPtr->setEntry(origChainPtr->getEntry());
+
         // Point to first node in new chain
-        Node<ItemType> *newChainPtr = topPtr;
+        Node<T> *newChainPtr = topPtr;
+
         // Copy remaining nodes
         while (origChainPtr != nullptr)
         {
             // Advance original-chain pointer
             origChainPtr = origChainPtr->getNext();
-            // Get next item from original chain
-            ItemType nextItem = origChainPtr->getItem();
-            // Create a new node containing the next item
-            Node<ItemType> *newNodePtr = new Node<ItemType>(nextItem);
+
+            // Get next entry from original chain
+            T nextEntry = origChainPtr->getEntry();
+
+            // Create a new node containing the next entry
+            Node<T> *newNodePtr = new Node<T>(nextEntry);
+
             // Link new node to end of new chain
             newChainPtr->setNext(newNodePtr);
+
             // Advance pointer to new last node
             newChainPtr = newChainPtr->getNext();
-        }                              // end while
-        newChainPtr->setNext(nullptr); // Flag end of chain
-    }                                  // end if
-} // end copy constructor
-template <class ItemType>
-LinkedStack<ItemType>::~LinkedStack()
+        }
+        newChainPtr->setNext(nullptr);
+    }
+}
+
+/** Destructor */
+template <class T>
+LinkedStack<T>::~LinkedStack()
 {
-    // Pop until stack is empty
+    // Keep popping until stack is empty
     while (!isEmpty())
         pop();
-} // end destructor
-template <class ItemType>
-bool LinkedStack<ItemType>::isEmpty() const
+}
+
+/** Determines if the stack is empty. */
+template <class T>
+bool LinkedStack<T>::isEmpty() const
 {
     return topPtr == nullptr;
-} // end isEmpty
-template <class ItemType>
-bool LinkedStack<ItemType>::push(const ItemType &newItem)
+}
+
+/** Adds a new entry to the top of the stack. */
+template <class T>
+bool LinkedStack<T>::push(const T &newEntry)
 {
-    Node<ItemType> *newNodePtr = new Node<ItemType>(newItem, topPtr);
+    Node<T> *newNodePtr = new Node<T>(newEntry, topPtr);
     topPtr = newNodePtr;
     newNodePtr = nullptr;
     return true;
-} // end push
-template <class ItemType>
-bool LinkedStack<ItemType>::pop()
+}
+
+/** Removes the top of this stack. */
+template <class T>
+bool LinkedStack<T>::pop()
 {
-    bool result = false;
-    if (!isEmpty())
+    bool result = !isEmpty();
+    if (result)
     {
-        // Stack is not empty; delete top
-        Node<ItemType> *nodeToDeletePtr = topPtr;
+        // Stack is not empty
+        Node<T> *nodeToDeletePtr = topPtr;
         topPtr = topPtr->getNext();
+
         // Return deleted node to system
         nodeToDeletePtr->setNext(nullptr);
         delete nodeToDeletePtr;
         nodeToDeletePtr = nullptr;
-        result = true;
-    } // end if
+    }
     return result;
-} // end pop
-template <class ItemType>
-ItemType LinkedStack<ItemType>::peek() const
+}
+
+/** Returns the top of this stack. */
+template <class T>
+T LinkedStack<T>::peek() const
 {
     assert(!isEmpty()); // Enforce precondition
-    // Stack is not empty; return top
-    return topPtr->getItem();
-} // end getTop
-// end of implementation file
+
+    return topPtr->getEntry();
+}
--- labs/lab02/LinkedStack.h
+++ labs/lab02/LinkedStack.h
@@ -1,25 +1,49 @@
-/** ADT stack: Link-based implementation.
-@file LinkedStack.h */
-#ifndef _LINKED_STACK
-#define _LINKED_STACK
+#ifndef _LINKED_STACK_H
+#define _LINKED_STACK_H
+
 #include "StackInterface.h"
 #include "Node.h"
-template <class ItemType>
-class LinkedStack : public StackInterface<ItemType>
+
+template <class T>
+class LinkedStack : public StackInterface<T>
 {
-private:
-    Node<ItemType> *topPtr; // Pointer to first node in the chain;
-    // this node contains the stack’s top
 public:
     // Constructors and destructor:
-    LinkedStack();                                    // Default constructor
-    LinkedStack(const LinkedStack<ItemType> &aStack); // Copy constructor
-    virtual ~LinkedStack();                           // Destructor
-    // Stack operations:
+    /** Default constructor */
+    LinkedStack();
+
+    /** Copy constructor */
+    LinkedStack(const LinkedStack<T> &aStack);
+
+    /** Destructor */
+    virtual ~LinkedStack();
+
+    /** Determines if the stack is empty.
+    @return True if the stack is empty, or false if not. */
     bool isEmpty() const;
-    bool push(const ItemType &newItem);
+
+    /** Adds a new entry to the top of the stack.
+    @post If the operation was successful, newEntry is at the top of the stack.
+    @param newEntry The object to be added to the top of the stack.
+    @return True if the addition is successful, false if not. */
+    bool push(const T &newEntry);
+
+    /** Removes the top of this stack.
+    @post If the operation was successful, the top of the stack
+    has been removed.
+    @return True if the removal is successful, false if not. */
     bool pop();
-    ItemType peek() const;
-}; // end LinkedStack
+
+    /** Returns the top of this stack.
+    @pre The stack is not empty.
+    @post The top of the stack has been returned, and
+    the stack is unchanged.
+    @return The top of the stack. */
+    T peek() const;
+
+private:
+    Node<T> *topPtr; // Pointer to first node in the chain
+};
+
 #include "LinkedStack.cpp"
-#endif
+#endif // _LINKED_STACK_H
