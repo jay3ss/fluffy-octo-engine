926f16bcdf3327ae1be4a684678dbd785eea2ebd
jay ess <mentoce@gmail.com>
2019-10-06 10:34:49 -0700

    Add tests for file reading

--- labs/lab02/main.cpp
+++ labs/lab02/main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 
+#include "FileReader.h"
 #include "PostfixEvaluator.h"
 
 using namespace std;
@@ -19,6 +20,41 @@ int main()
 
     double result = evaluator.evaluate(expr);
     cout << "Result is " << result << endl;
+
+    cout << "Testing the FileReader class\n";
+
+    cout << "Going to pen a non-existent file\n";
+
+    FileReader filereader;
+
+    try
+    {
+        filereader.open("dne.txt");
+    }
+    catch (FileNotFoundException)
+    {
+        cerr << "ERROR: File does not exist.\n\n";
+    }
+
+    cout << "Going to open an existent file\n";
+
+    try
+    {
+        filereader.open("../expressions.txt");
+    }
+    catch (FileNotFoundException)
+    {
+        cerr << "ERROR: File does not exist.\n";
+    }
+
+    // Test the reading of lines of the file until the end of the file
+    expr = filereader.getline();
+    while (filereader.isEof())
+    {
+        result = evaluator.evaluate(expr);
+        cout << expr << " = " << result << endl;
+        expr = filereader.getline();
+    }
 
     return 0;
 }
