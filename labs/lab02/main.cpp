e3b6d298e9175b57c1a761be5cce9f4cfeb76320
jay ess <mentoce@gmail.com>
2019-10-06 20:51:03 -0700

    Update main program

--- labs/lab02/main.cpp
+++ labs/lab02/main.cpp
@@ -7,54 +7,53 @@
 
 using namespace std;
 
+bool openFile(FileReader &reader, string fileName);
+
 int main()
 {
     PostfixEvaluator evaluator;
-
-    // string expr = "1 2 +";
-    // string expr = "2 3 + 7 *";
-    // string expr = "2 3 4 + *";
-    // string expr = "5 3 2 * + 4 - 5 +";
-    // string expr = "6 3 + 2 *";
-    string expr = "2 1 3 + *";
-
-    double result = evaluator.evaluate(expr);
-    cout << "Result is " << result << endl;
-
-    cout << "Testing the FileReader class\n";
-
-    cout << "Going to pen a non-existent file\n";
-
     FileReader filereader;
 
-    try
-    {
-        filereader.open("dne.txt");
-    }
-    catch (FileNotFoundException)
-    {
-        cerr << "ERROR: File does not exist.\n\n";
-    }
 
-    cout << "Going to open an existent file\n";
+    string fileName = "../expressions.txt";
+    openFile(filereader, fileName);
 
-    try
-    {
-        filereader.open("../expressions.txt");
-    }
-    catch (FileNotFoundException)
-    {
-        cerr << "ERROR: File does not exist.\n";
-    }
+    cout << "Evaluating expressions file\n"
+         << "---------------------------\n";
 
     // Test the reading of lines of the file until the end of the file
-    expr = filereader.getline();
+    string expr = filereader.getline();
     while (filereader.isEof())
     {
-        result = evaluator.evaluate(expr);
-        cout << expr << " = " << result << endl;
+        try
+        {
+            double result = evaluator.evaluate(expr);
+            cout << expr << " = " << result << endl;
+        }
+        catch (InvalidExpressionException)
+        {
+            cerr << "ERROR: Invalid expression\n";
+        }
+        catch (DivisionByZeroException)
+        {
+            cerr << "ERROR: Division by zero.\n";
+        }
         expr = filereader.getline();
     }
 
     return 0;
+}
+
+bool openFile(FileReader &reader, string fileName)
+{
+    try
+    {
+        reader.open(fileName);
+        return true;
+    }
+    catch (FileNotFoundException)
+    {
+        cerr << "ERROR: File does not exist.\n";
+        return false;
+    }
 }
