edc144af1eba9f63940019645aea3b74a8402e71
jay ess <mentoce@gmail.com>
2019-10-06 20:51:22 -0700

    Update comments

--- labs/lab02/PostfixEvaluator.h
+++ labs/lab02/PostfixEvaluator.h
@@ -33,19 +33,31 @@ public:
     @returns    The result of the postfix expression */
     double evaluate(std::string expression);
 private:
-    /** Determines if a token is an operand */
-    bool isOperand(std::string token);
-
-    /** Determines if a token is an operand: +, -, *, or / */
+    /** Determines if a token is an operand: +, -, *, or /
+    @param token The token to be checked
+    @returns True if the token is an operator, false otherwise */
     bool isOperator(std::string token);
+
+    /** Converts a token to an operand (double)
+    @pre        The token should be able to be converted to a double
+    @param      token The token to be converted
+    @returns    Returns a an operand (as a double) */
+    double convertToOperand(std::string token);
 
     /** Evaluates a single postfix operation
     @pre    If operand is division ('/'), then op2 cannot equal zero
-    @param  op1 First operand*/
+    @param  op1 First operand
+    @param  op2 Second operand
+    @param opertr The operation that's to be performed
+    @returns    The result of the evaluation of the operation */
     double evalOperation(double op1, double op2, char opertr);
 
     /** Splits the expression into a vector of tokens (operators and operands)
-    and returns the token vector */
+    and returns the token vector
+    @pre    expression should be a well-formed postfix expression
+    @param  expression The postfix expression
+    @returns A vector containing the tokens that are the operands and operators
+    of the postfix expression */
     std::vector<std::string> tokenize(std::string expression);
 };
 
